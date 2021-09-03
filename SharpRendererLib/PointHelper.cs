171db52af289fc1756615f9c6ae1956b68d5a28d
Taylor Britton <tbrit10@gmail.com>
2021-09-02 18:07:12 -0700

    Add PointHelper class containing simple Point helper methods

---
+++ SharpRendererLib/PointHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace SharpRendererLib
+{
+    public static class PointHelper
+    {
+        public static bool IsOutOfBounds(int width, int height, Point point)
+        {
+            return point.X >= width ||
+                   point.Y >= height;
+        }
+
+        public static void SwapPoints(ref Point point1, ref Point point2)
+        {
+            (point1, point2) = (point2, point1);
+        }
+    }
+}
---
+++ SharpRendererLibTests/PointHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using SharpRendererLib;
+
+namespace SharpRendererLibTests
+{
+    [TestFixture]
+    public class PointHelperTests
+    {
+        [Test]
+        [TestCase(10, 10, 11, 11)]
+        [TestCase(10, 10, 10, 10)]
+        [TestCase(10, 11, 11, 1)]
+        [TestCase(10, 10, 1, 11)]
+        [TestCase(10, 10, 10, 1)]
+        [TestCase(10, 10, 1, 10)]
+        public void IsOutOfBounds_PointOutOfBounds_ReturnsTrue(int width, int height, int x, int y)
+        {
+            Point point = new Point(x, y);
+            Assert.IsTrue(PointHelper.IsOutOfBounds(width, height, point));
+        }
+        
+        [Test]
+        [TestCase(1, 1, 2, 2)]
+        [TestCase(2, 2, 1, 1)]
+        public void SwapPoints_ValuesAreSwapped(int x1, int y1, int x2, int y2)
+        {
+            Point point1 = new Point(x1, y1);
+            Point point2 = new Point(x2, y2);
+            
+            PointHelper.SwapPoints(ref point1, ref point2);
+            Assert.AreEqual(x1, point2.X);
+            Assert.AreEqual(y1, point2.Y);
+            Assert.AreEqual(x2, point1.X);
+            Assert.AreEqual(y2, point1.Y);
+        }
+    }
+}

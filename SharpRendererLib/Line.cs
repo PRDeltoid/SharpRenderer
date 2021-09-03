995f467443726f5042303e4a67710b8f45b29bac
Taylor Britton <tbrit10@gmail.com>
2021-09-02 20:55:05 -0700

    Add Line class
    - Add Line class to encapsulate values related to a line formed by two points

---
+++ SharpRendererLib/Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace SharpRendererLib
+{
+    public class Line
+    {
+        public double Slope { get; }
+        public bool IsSteep { get; }
+        public Point Point1 { get; }
+        public Point Point2 { get; }
+
+        public Line(Point point1, Point point2)
+        {
+            // Make sure the points can form a line
+            if (point1 == point2) throw new Exception("Failed to create line. Both points are the same."); 
+            
+            Point1 = point1;
+            Point2 = point2;
+            
+            int xAbs = PointHelper.DistanceX(point1, point2);
+            int yAbs = PointHelper.DistanceY(point1, point2);
+            IsSteep = xAbs < yAbs; 
+            
+            // Our line object always holds our line in left-to-right orientation (smallest X to largest X)
+            if (point1.X > point2.X)
+            {
+                // This swap ensures that point1 is always further left
+                PointHelper.SwapPoints(ref point1, ref point2);
+            }
+            
+
+            Slope = PointHelper.CalculateSlope(point1, point2);
+        }
+    }
+}
---
+++ SharpRendererLibTests/LineTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using SharpRendererLib;
+
+namespace SharpRendererLibTests
+{
+    [TestFixture]
+    public class LineTests
+    {
+        [Test]
+        [TestCase(1, 1, 1, 1)]
+        [TestCase(2, 2, 2, 2)]
+        [TestCase(3, 3, 3, 3)]
+        public void ThrowsWhen_PointsAreTheSame(int x1, int y1, int x2, int y2)
+        {
+            Assert.Throws<Exception>(() => new Line(new Point(x1, x1), new Point(x2, y2)));
+        }
+    }
+}

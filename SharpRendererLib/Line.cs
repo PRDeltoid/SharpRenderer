35bfae7c7c9771e11e638f89b9148d849bd2068b
Taylor Britton <tbrit10@gmail.com>
2021-09-05 11:13:05 -0700

    Remove IsSteep property from Line class
    - Remove IsSteep property as it belongs more in an implementation of a line drawing algo than in the line itself

--- SharpRendererLib/Line.cs
+++ SharpRendererLib/Line.cs
@@ -6,7 +6,6 @@ namespace SharpRendererLib
     public class Line
     {
         public double Slope { get; }
-        public bool IsSteep { get; }
         public Point Point1 { get; }
         public Point Point2 { get; }
 
@@ -17,10 +16,6 @@ namespace SharpRendererLib
             
             Point1 = point1;
             Point2 = point2;
-            
-            int xAbs = PointHelper.DistanceX(point1, point2);
-            int yAbs = PointHelper.DistanceY(point1, point2);
-            IsSteep = xAbs < yAbs; 
             
             // Our line object always holds our line in left-to-right orientation (smallest X to largest X)
             if (point1.X > point2.X)

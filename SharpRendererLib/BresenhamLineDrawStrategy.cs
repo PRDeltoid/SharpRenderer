1673d62582c518f336f8178e9599bb0bbeae974d
Taylor Britton <tbrit10@gmail.com>
2021-09-05 18:23:12 -0700

    Improve polygon drawing
    - Improve drawing of steep lines from both top-left to bottom-right and bottom-left to top-right configurations

--- SharpRendererLib/BresenhamLineDrawStrategy.cs
+++ SharpRendererLib/BresenhamLineDrawStrategy.cs
@@ -7,21 +7,25 @@ namespace SharpRendererLib
     {
         public void CallOnAllDrawPoints(Line line, Action<int, int> drawFunc)
         {
-            bool isSteep = line.Slope > 1;
-
             Point p1 = line.Point1;
             Point p2 = line.Point2;
+            
             if (p1.X > p2.X)
             {
                 throw new Exception($"Line is pointing the wrong direction. P1: ({p1.X},{p1.Y}),  P2:({p2.X},{p2.Y}).");
             }
 
+            bool isSteep = line.Slope is > 1 or < -1;
             if (isSteep)
             {
                 // This algorithm works better with a shallower line
                 // We can transpose a steep line to make it shallow, run the algo, and then swap it back at draw-time
                 PointHelper.TransposeXY(ref p1);
                 PointHelper.TransposeXY(ref p2);
+                if (line.Slope < -1)
+                {
+                    PointHelper.SwapPoints(ref p1, ref p2);
+                }
             }
 
             // If our line is a single point, just draw the point and exit without looping
@@ -35,8 +39,12 @@ namespace SharpRendererLib
             {
                 // How far we've "traveled" down the line
                 int dist = PointHelper.DistanceX(p1, p2);
-                int deltaX = drawX - line.Point1.X;
+                int deltaX = drawX - p1.X;
                 double time = deltaX / (double)dist;
+                if (double.IsNaN(time))
+                {
+                    time = 0.0;
+                }
                 int drawY = (int)(p1.Y * (1.0 - time) + p2.Y * time);
 
                 if (isSteep)
--- SharpRendererLib/WireMeshPolygonDrawer.cs
+++ SharpRendererLib/WireMeshPolygonDrawer.cs
@@ -12,37 +12,46 @@ namespace SharpRendererLib
             _lineDrawer = new LineDrawer(lineDrawStrat);
         }
         
-        public void Draw(PixelBuffer pixelBuffer, Polygon polygon, Color color)
+        public void Draw(PixelBuffer pixelBuffer, Polygon polygon, int width, int height, Point startPoint, Color color)
         {
-            int halfHeight = (int)(pixelBuffer.Height / 2.0);
-            int halfWidth = (int)(pixelBuffer.Width / 2.0);
+            int halfWidth = (int)(width / 2.0);
+            int halfHeight = (int)(height / 2.0);
             foreach (Face face in polygon.Faces)
             {
-                DrawFace(pixelBuffer, polygon, face, color, halfWidth, halfHeight);
+                DrawFace(pixelBuffer, polygon, face, color, halfWidth, halfHeight, startPoint);
             }
         }
 
-        private void DrawFace(PixelBuffer pixelBuffer, Polygon polygon, Face face, Color color, int halfWidth, int halfHeight)
+        private void DrawFace(PixelBuffer pixelBuffer, Polygon polygon, Face face, Color color, int halfWidth, int halfHeight, Point startPoint)
         {
             Vertex vert1 = polygon.Vertices[face.Indices[0].Vertex];
             Vertex vert2 = polygon.Vertices[face.Indices[1].Vertex];
             Vertex vert3 = polygon.Vertices[face.Indices[2].Vertex];
-            DrawVertexLine(pixelBuffer, vert1, vert2, color, halfWidth, halfHeight);
-            DrawVertexLine(pixelBuffer,vert2, vert3, color, halfWidth, halfHeight);
-            DrawVertexLine(pixelBuffer,vert3, vert1, color, halfWidth, halfHeight);
+            
+            // Line 1 -> 2
+            DrawVertexLine(pixelBuffer, vert1, vert2, color, halfWidth, halfHeight, startPoint);
+            // Line 2 -> 3
+            DrawVertexLine(pixelBuffer,vert2, vert3, color, halfWidth, halfHeight, startPoint);
+            // Line 3 -> 1
+            DrawVertexLine(pixelBuffer,vert3, vert1, color, halfWidth, halfHeight, startPoint);
         }
 
-        private void DrawVertexLine(PixelBuffer pixelBuffer, Vertex vert1, Vertex vert2, Color color, int halfWidth, int halfHeight)
+        private void DrawVertexLine(PixelBuffer pixelBuffer, Vertex vert1, Vertex vert2, Color color, int halfWidth, int halfHeight, Point startPoint)
         {
-            int x1 = (int)((vert1.X + 1) * (halfWidth-1));
-            int y1 = (int)((vert1.Y + 1) * (halfHeight-1));
-            int x2 = (int)((vert2.X + 1) * (halfWidth-1));
-            int y2 = (int)((vert2.Y + 1) * (halfHeight-1));
+            (int x1, int y1) = VertexToPixel(vert1, halfWidth, halfHeight, startPoint);
+            (int x2, int y2) = VertexToPixel(vert2, halfWidth, halfHeight, startPoint);
             Point vertPoint1 = new Point(x1, y1);
             Point vertPoint2 = new Point(x2, y2);
             Line line = new Line(vertPoint1, vertPoint2);
                 
             _lineDrawer.DrawLine(pixelBuffer, line, color);  
+        }
+
+        private (int, int) VertexToPixel(Vertex vert, int halfWidth, int halfHeight, Point startPoint)
+        {
+            int x = (int)((vert.X + 1) * (halfWidth-1));
+            int y = (int)((vert.Y + 1) * (halfHeight-1));
+            return (x + startPoint.X, y + startPoint.Y);
         }
     }
 }

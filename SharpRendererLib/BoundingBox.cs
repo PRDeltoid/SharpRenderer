26e088af5417d91db4b8069737c5886dbb1a6050
Taylor Britton <tbrit10@gmail.com>
2021-09-10 15:29:13 -0700

    Change PerformActionOverBB method parameter from int,int to Point
    - Encapsulate X,Y parameters in the PerformActionOverBoundingBox delegate action in a Point struct

--- SharpRendererLib/BoundingBox.cs
+++ SharpRendererLib/BoundingBox.cs
@@ -14,13 +14,13 @@ namespace SharpRendererLib
         public Point TopLeft { get; }
         public Point BottomRight { get; }
 
-        public void PerformActionOverBoundingBox(Action<int, int> action)
+        public void PerformActionOverBoundingBox(Action<Point> action)
         {
             // Iterate over the entire bounding box
             for (int x = TopLeft.X; x < BottomRight.X; x++)
             for (int y = TopLeft.Y; y < BottomRight.Y; y++)
                 // Run our provided function on our current point in the bounding box
-                action.Invoke(x, y);
+                action.Invoke(new Point(x, y));
         }
     }
 }

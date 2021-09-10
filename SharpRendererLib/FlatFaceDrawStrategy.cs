1fce56f7f27256bfb6ca3e40e67c2bb2064f5c5a
Taylor Britton <tbrit10@gmail.com>
2021-09-10 16:47:52 -0700

    Add flat face draw strategy
    - Add a strategy for drawing faces in flat colors (ie. without shadow or texture, a single color per face). Supports FlatColorDraw strategies to pick face color.

---
+++ SharpRendererLib/FlatFaceDrawStrategy.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using SharpRendererLib.Helpers;
+
+namespace SharpRendererLib
+{
+    public class FlatFaceDrawStrategy : IFaceDrawStrategy
+    {
+        private readonly IFlatColorDrawStrategy _colorDrawStrategy;
+
+        public FlatFaceDrawStrategy(IFlatColorDrawStrategy colorDrawStrategy)
+        {
+            _colorDrawStrategy = colorDrawStrategy;
+        }
+        
+        public void DrawFace(PixelBuffer pixelBuffer, Polygon polygon, Face face, int width, int height, Point start)
+        {
+            Color faceColor = _colorDrawStrategy.GetColor();
+            
+            Triangle faceTriangle = TriangleHelper.TriangleFromFace(face, width, height);
+            BoundingBox bb = BoundingBoxHelper.GetBoundingBox(faceTriangle.Points);
+
+            bb.PerformActionOverBoundingBox(((point) =>
+            {
+                if (TriangleHelper.PointInTriangle(faceTriangle, point))
+                {
+                    var drawPoint = PointHelper.OffsetPoint(point, start);
+                    pixelBuffer.SetPixel(drawPoint.X, drawPoint.Y, faceColor);
+                }
+            }));
+        }
+    }
+}

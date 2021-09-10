670692bf5b9dbddb15a6fba5a3607b91bc1c9eb3
Taylor Britton <tbrit10@gmail.com>
2021-09-10 16:46:14 -0700

    Add Random Color Draw strat
    - Add a color draw strategy which generates a random color each time GetColor() is called

---
+++ SharpRendererLib/RandomColorDrawStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+
+namespace SharpRendererLib
+{
+    public class RandomColorDrawStrategy : IFlatColorDrawStrategy
+    {
+        private static readonly Random Rand = new Random();
+        
+        public Color GetColor()
+        {
+            return Color.FromArgb(Rand.Next(0, 256), 
+                Rand.Next(0, 256), Rand.Next(0, 256));
+        }
+    }
+}

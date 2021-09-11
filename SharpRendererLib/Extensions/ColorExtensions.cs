5ee4ced5761b429b2f8b087d8047a834e006d49f
Taylor Britton <tbrit10@gmail.com>
2021-09-10 18:50:06 -0700

    Add color extension to set a color's intensity
    - Add extension method to Color type which changes the intensity of the color by a floating point amount

---
+++ SharpRendererLib/Extensions/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace SharpRendererLib.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static void SetIntensity(this ref Color color, float intensity)
+        {
+            color = Color.FromArgb(color.A, (int)(color.R * intensity), (int)(color.G * intensity), (int)(color.B * intensity));  
+        }
+    }
+}

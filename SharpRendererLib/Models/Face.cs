266c7fb149710b07a566dd6754d542193b40dc31
Taylor Britton <tbrit10@gmail.com>
2021-09-10 20:23:08 -0700

    Add helper method to get all vertexes from face as array

--- SharpRendererLib/Models/Face.cs
+++ SharpRendererLib/Models/Face.cs
@@ -7,10 +7,12 @@ namespace SharpRendererLib.Models
         public Vertex Vertex3 { get; }
         public Vertex Vertex2 { get; }
         public Vertex Vertex1 { get; }
+        public Vertex[] Vertexes { get; }
         
         public Face(Vertex vertex1, Vertex vertex2, Vertex vertex3)
         {
             (Vertex1, Vertex2, Vertex3) = (vertex1, vertex2, vertex3);
+            Vertexes = new Vertex[] { Vertex1, Vertex2, Vertex3 };
         }
     }
 }

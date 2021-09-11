f8befe4cdaa7f88adb1408e057137a7250be99a0
Taylor Britton <tbrit10@gmail.com>
2021-09-10 19:20:52 -0700

    Create a Light which typedefs Vector3
    - Create a light class which is essentially a typedef over Vector3

---
+++ SharpRendererLib/Models/Light.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace SharpRendererLib.Models
+{
+    public class Light
+    {
+        private Vector3 _value;
+
+        private Light(Vector3 value)
+        {
+            _value = value;
+        }
+        
+        public static implicit operator Light(Vector3 value)
+        {
+            return new Light(value);
+        }
+        
+        public static implicit operator Vector3(Light light)
+        {
+            return light._value;
+        }
+    }
+}

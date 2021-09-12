29cc0ea07f9e714d5ad8c95c3f6b2c5e2effe6e7
Taylor Britton <tbrit10@gmail.com>
2021-09-12 10:39:46 -0700

    Add Texture class
    - Add a texture class which wraps around a bitmap

---
+++ SharpRendererLib/Models/Texture.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace SharpRendererLib.Models
+{
+    public class Texture
+    {
+        private readonly Bitmap _texture;
+
+        public int Width => _texture.Width;
+        public int Height => _texture.Height;
+
+        public Texture(Bitmap texture)
+        {
+            _texture = texture;
+        }
+
+        public Color GetPointColor(int x, int y)
+        {
+            return _texture.GetPixel(x, y);
+        }
+    }
+}

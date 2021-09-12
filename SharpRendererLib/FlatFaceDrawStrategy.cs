3d5a93c37fc0adfb4bb026eeadc31e52a8bf3fe4
Taylor Britton <tbrit10@gmail.com>
2021-09-11 18:26:39 -0700

    Add Z buffer support to FlatFaceDrawStrategy
    - Add support for a ZBuffer, which helps make sure that points in the foreground are not overwritten by points in the background.

--- SharpRendererLib/FlatFaceDrawStrategy.cs
+++ SharpRendererLib/FlatFaceDrawStrategy.cs
@@ -1,8 +1,10 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using System.Numerics;
 using SharpRendererLib.Extensions;
 using SharpRendererLib.Helpers;
 using SharpRendererLib.Models;
+using Face = SharpRendererLib.Models.Face;
 
 namespace SharpRendererLib
 {
@@ -40,12 +42,39 @@ namespace SharpRendererLib
             
             bb.PerformActionOverBoundingBox(((point) =>
             {
-                if (TriangleHelper.PointInTriangle(faceTriangle, point))
-                {
-                    Point drawPoint = PointHelper.OffsetPoint(point, start);
-                    pixelBuffer.SetPixel(drawPoint.X, drawPoint.Y, faceColor);
-                }
+                // If where we're drawing is negative, skip it as it is outside the screen
+                Point drawPoint = PointHelper.OffsetPoint(point, start);
+                if(drawPoint.X < 0 || drawPoint.Y < 0 || drawPoint.X >= width+start.X || drawPoint.Y >= height+start.Y) return;
+                
+                // Check if our barycentric coordinate indicates that the point is within the bounds of faceTriangle
+                // If it is out of bounds, skip drawing it.
+                Vector3 bc = TriangleHelper.Barycentric(faceTriangle, point);
+                if (BarycentricPointInTriangle(bc) == false) return;
+                
+                // If we've already drawn something closer to the camera already, skip drawing this point
+                float z = CalculatePointZ(face, bc);
+                if (zBuffer.TrySetZ(drawPoint.X, drawPoint.Y, z) == false) return;
+
+                // If we get here, we've passed all of our tests and should draw the point
+                pixelBuffer.SetPixel(drawPoint.X, drawPoint.Y, faceColor);
             }));
+        }
+
+        private static bool BarycentricPointInTriangle(Vector3 barycentricPoint)
+        {
+            if (barycentricPoint.X < 0 || barycentricPoint.Y < 0 || barycentricPoint.Z < 0) return false;
+
+            return true; 
+        }
+
+        private static float CalculatePointZ(Face face, Vector3 barycentric)
+        {
+            float z = 0;
+            // Interpolate Z value of the current point we are drawing using its barycentric coordinates
+            z += face.Vertex1.Z * barycentric.X;
+            z += face.Vertex2.Z * barycentric.Y;
+            z += face.Vertex3.Z * barycentric.Z;
+            return z;
         }
     }
 }

8730efba9e68994f8f75cda0f8292b0de4bce43c
Taylor Britton <tbrit10@gmail.com>
2021-09-30 16:01:56 -0700

    Add MatrixHelper helper class
    - Add a MatrixHelper class which currently contains a method for transforming matrixes into vertexes

---
+++ SharpRendererLib/Helpers/MatrixHelper.cs
@@ -0,0 +1,18 @@
+﻿using SharpGL.SceneGraph;
+using Matrix = SharpRendererLib.Models.Matrix;
+
+namespace SharpRendererLib.Helpers
+{
+    public class MatrixHelper
+    {
+        public static Vertex MatrixToVertex(Matrix matrix)
+        {
+            Vertex vertex = new Vertex();
+            float div = (float)matrix[3,0];
+            vertex.X = (float)matrix[0, 0] / div;
+            vertex.Y = (float)matrix[1, 0] / div;
+            vertex.Z = (float)matrix[2, 0] / div;
+            return vertex;
+        }
+    }
+}

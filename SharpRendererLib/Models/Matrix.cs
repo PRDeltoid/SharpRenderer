d14b4adf3472806ac91b020c6acacc563870893c
Taylor Britton <tbrit10@gmail.com>
2021-09-30 15:58:14 -0700

    Add Matrix wrapper
    - Add a wrapper class around SharpGL's Matrix class

---
+++ SharpRendererLib/Models/Matrix.cs
@@ -0,0 +1,46 @@
+﻿namespace SharpRendererLib.Models
+{
+    public class Matrix
+    {
+        private readonly SharpGL.SceneGraph.Matrix _matrix;
+
+        public Matrix(int rows, int cols)
+        {
+            _matrix = new SharpGL.SceneGraph.Matrix(rows, cols);
+        }
+
+        private Matrix(SharpGL.SceneGraph.Matrix matrix)
+        {
+            _matrix = matrix;
+        }
+        
+        public double this[int i, int j] {
+            get => _matrix[i, j];
+            set => _matrix[i, j] = value;
+        }
+        
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            return new Matrix(matrix1._matrix * matrix2._matrix);
+        }
+        
+        public static Matrix Identity(int width)
+        {
+            Matrix matrix = new Matrix(width, width);
+            // An identity matrix has '1's along the top-left to bottom-right diagonal
+            // All other spaces are '0'
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}

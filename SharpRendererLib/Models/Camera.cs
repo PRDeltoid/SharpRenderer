bbdf6b1bc31942230bf4d012db32b9d89b202708
Taylor Britton <tbrit10@gmail.com>
2021-09-30 15:58:44 -0700

    Add Camera class
    - Add a Camera class, which is a simple wrapper around a Vector3

---
+++ SharpRendererLib/Models/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace SharpRendererLib.Models
+{
+    public class Camera
+    {
+        private readonly Vector3 _vector;
+
+        public Camera(float x, float y, float z)
+        {
+            _vector = new Vector3(x, y, z);
+        }
+
+        public float X => _vector.X;
+        public float Y => _vector.Y;
+        public float Z => _vector.Z;
+    }
+}

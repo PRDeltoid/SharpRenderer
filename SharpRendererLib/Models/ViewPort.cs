16475775729a302161a3de3eb2e461c87941dccc
Taylor Britton <tbrit10@gmail.com>
2021-09-30 16:00:35 -0700

    Add ViewPort class
    - Add a ViewPort class which uses matrix transformations to "project" a vertex onto the ViewPort. Using a ViewPort allows us to give our vertexes realistic perspective

---
+++ SharpRendererLib/Models/ViewPort.cs
@@ -0,0 +1,38 @@
+﻿namespace SharpRendererLib.Models
+{
+    public class ViewPort
+    {
+        private readonly Matrix _matrix;
+        
+        public int Width { get; }
+        public int Height { get; }
+        
+        public ViewPort(int width, int height, int depth, int x, int y)
+        {
+            Width = width;
+            Height = height;
+            _matrix = ConstructViewport(x, y, width / 8, height / 8, depth);
+        }
+
+        private static Matrix ConstructViewport(int x, int y, int width, int height, int depth)
+        {
+            float halfWidth = width / 2f;
+            float halfHeight = height / 2f;
+            float halfDepth = depth / 2f;
+            Matrix matrix = Matrix.Identity(4);
+            matrix[0, 3] = x + halfWidth;
+            matrix[1, 3] = y + halfHeight;
+            matrix[2, 3] = halfDepth;
+
+            matrix[0, 0] = halfWidth;
+            matrix[1, 1] = halfHeight;
+            matrix[2, 2] = halfDepth;
+            return matrix;
+        }
+
+        public static Matrix operator *(ViewPort vp, Matrix matrix)
+        {
+            return vp._matrix* matrix;
+        }
+    }
+}

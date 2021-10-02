768749b28fa0b35d09a61ff295ae08ebfd7b5606
Taylor Britton <tbrit10@gmail.com>
2021-10-02 14:09:22 -0700

    Remove tests of old methods
    - Remove test of old vertex/triangle transformation methods. These have been replaced with Matrix transformations.

--- SharpRendererLibTests/TriangleHelperTests.cs
+++ SharpRendererLibTests/TriangleHelperTests.cs
@@ -1,30 +1,13 @@
 ﻿using System.Drawing;
 using NUnit.Framework;
-using SharpGL.SceneGraph;
 using SharpRendererLib.Helpers;
 using SharpRendererLib.Models;
-using Face = SharpRendererLib.Models.Face;
 
 namespace SharpRendererLibTests
 {
     [TestFixture]
     public class TriangleHelperTests
     {
-        [Test]
-        public void TriangleFromFace_ReturnsCorrectValues()
-        {
-            Face face = new Face(new Vertex(0,0,0), new Vertex(1,1,0), new Vertex(-1, 0, 0));
-            Triangle triangle = TriangleHelper.TriangleFromFace(face, 10, 10);
-            Assert.AreEqual(0, triangle.Point1.X);
-            Assert.AreEqual(0, triangle.Point1.Y);
-            
-            Assert.AreEqual(5, triangle.Point2.X);
-            Assert.AreEqual(5, triangle.Point2.Y);
-            
-            Assert.AreEqual(5, triangle.Point3.X);
-            Assert.AreEqual(0, triangle.Point3.Y);
-        }
-
         [Test]
         public void PointInTriangle_ReturnsCorrectValues()
         {
--- SharpRendererLibTests/VertexHelperTests.cs
+++ SharpRendererLibTests/VertexHelperTests.cs
@@ -1,25 +1,9 @@
-﻿using System.Drawing;
-using NUnit.Framework;
-using SharpGL.SceneGraph;
-using SharpRendererLib.Helpers;
+﻿using NUnit.Framework;
 
 namespace SharpRendererLibTests
 {
     [TestFixture]
     public class VertexHelperTests
     {
-        [Test]
-        [TestCase(1, 1, 10, 10, 5, 5)]
-        [TestCase(2, 2, 10, 10, 10, 10)]
-        [TestCase(5, 5, 10, 10, 25, 25)]
-        public void VertexToPoint_ReturnsCorrectValues (float vx, float vy, int width, int height, int expectedX, int expectedY)
-        {
-            Vertex vert = new Vertex(vx, vy, 0);
-
-            Point point = VertexHelper.VertexToPoint(vert, width / 2, height / 2);
-
-            Assert.AreEqual(expectedX, point.X); 
-            Assert.AreEqual(expectedY, point.Y); 
-        }
     }
 }

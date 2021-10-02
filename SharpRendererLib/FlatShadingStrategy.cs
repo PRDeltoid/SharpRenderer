4b94451874222ec3fae46400bf704b6206616bd2
Taylor Britton <tbrit10@gmail.com>
2021-10-01 20:58:40 -0700

    Add Shade Drawing Strategy interface
    - Add shade drawing interface and flat shading implementation

---
+++ SharpRendererLib/FlatShadingStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using SharpRendererLib.Helpers;
+using SharpRendererLib.Models;
+
+namespace SharpRendererLib
+{
+    public class FlatShadingStrategy : IShadingStrategy
+    {
+        public float GetIntensity(Face face, Vector3 _barycentricCoord, Light light)
+        {
+            // Get a vector that is normal (orthagonal) to the face
+            Vector3 faceNormal = FaceHelper.GetFaceNormal(face);
+            
+            // Determine the intensity of light hitting the face
+            float intensity = Vector3.Dot(faceNormal, light);
+            return intensity;
+        }
+
+        // Flat shading is always per-face, not per-point
+        public bool ShadePerFace => true;
+    }
+}
---
+++ SharpRendererLib/IShadingStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using SharpRendererLib.Models;
+
+namespace SharpRendererLib
+{
+    public interface IShadingStrategy
+    {
+        float GetIntensity(Face face, Vector3 barycentricCoord, Light light);
+        bool ShadePerFace { get; }
+    }
+}
